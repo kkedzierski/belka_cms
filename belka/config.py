685c262a5e327485b3aeb8ddb1a847fc514aa5bf
kkedzierski <kkedzierski@edu.cdv.pl>
2020-05-18 20:34:09 +0200

    Create signing functionality

--- belka/config.py
+++ belka/config.py
@@ -2,5 +2,6 @@ import os
 
 
 class Config:
+    SECRET_KEY = os.environ.get('BELKA_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
